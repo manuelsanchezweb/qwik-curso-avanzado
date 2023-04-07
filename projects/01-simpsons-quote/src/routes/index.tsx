07b9d405d261bf3088231cfaf98308829311f141
manuelsanchez2 <manusansan22@gmail.com>
2023-04-07 16:05:44 +0200

    🆕 feat: Create first screen

--- projects/01-simpsons-quote/src/routes/index.tsx
+++ projects/01-simpsons-quote/src/routes/index.tsx
@@ -1,11 +1,39 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { DocumentHead, Link } from "@builder.io/qwik-city";
+import { Logo } from "~/components/icons/logo";
 
 export default component$(() => {
   return (
-    <>
-      <h1 class="text-5xl">The Simpsons App</h1>
-    </>
+    <div class="flex flex-col min-h-screen items-center justify-center text-center bg-brand p-4">
+      <Logo classCustom="w-64 sm:w-[35rem]" />
+      <h1 class="text-xl my-1 font-medium">
+        Discover some of the most amazing Quotes from The Simpsons
+      </h1>
+      <div>
+        <strong>DISCLAIMER:</strong> This is an unofficial app (even though it
+        looks so cool!)
+      </div>
+      <Link class="btn my-4" href="/">
+        Enter App
+      </Link>
+      <div class="flex gap-12">
+        <img
+          class="hidden md:block object-contain"
+          src="./img/bart.png"
+          alt="Bart Simpson"
+        />
+        <img
+          class="hidden md:block object-contain mt-12"
+          src="./img/homer.png"
+          alt="Homer Simpson"
+        />
+        <img
+          class="object-contain mt-12 md:mt-0"
+          src="./img/moe.png"
+          alt="Moe"
+        />
+      </div>
+    </div>
   );
 });
 
