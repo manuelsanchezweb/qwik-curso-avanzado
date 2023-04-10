59f1004b388b4259f1934fab8408ead70b95c7d2
manuelsanchez2 <manusansan22@gmail.com>
2023-04-10 17:36:28 +0200

    🆕 feat: Finish app

--- projects/02-weather-app/src/routes/index.tsx
+++ projects/02-weather-app/src/routes/index.tsx
@@ -22,11 +22,59 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Weather App",
+  title: "Aplicación del tiempo con Qwik",
   meta: [
     {
       name: "description",
-      content: "Descubre el tiempo de tu ciudad favorita con nuestra app",
+      content:
+        "Con esta aplicación podrás ver en tiempo real cuál es el tiempo de la ciudad que quieras.",
+    },
+    {
+      name: "keywords",
+      content:
+        "aplicación del tiempo, pronósticos precisos, ubicación actual, ciudades del mundo, planificar tu día, Qwik",
+    },
+    {
+      name: "author",
+      content: "manuelsanchezweb",
+    },
+    {
+      property: "og:title",
+      content: "☀️ Aplicación del tiempo con Qwik",
+    },
+    {
+      property: "og:description",
+      content:
+        "Qwik es una aplicación del tiempo que te ofrece pronósticos precisos y actualizados para tu ubicación actual y otras ciudades del mundo. ¡Descubre cómo Qwik puede ayudarte a planificar tu día de manera efectiva!",
+    },
+    {
+      property: "og:image",
+      content: "https://weather-qwik-curso-avanzado.vercel.app/thumbnail.png", // replace with actual image URL from Unsplash
+    },
+    {
+      property: "og:url",
+      content: "https://weather-qwik-curso-avanzado.vercel.app/", // replace with actual URL of the Qwik site
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "twitter:site",
+      content: "@manuelsandoble", // replace with your Twitter handle
+    },
+    {
+      name: "twitter:title",
+      content: "☀️ Aplicación del tiempo con Qwik",
+    },
+    {
+      name: "twitter:description",
+      content:
+        "Con esta aplicación podrás ver en tiempo real cuál es el tiempo de la ciudad que quieras.",
+    },
+    {
+      name: "twitter:image",
+      content: "https://weather-qwik-curso-avanzado.vercel.app/thumbnail.png", // replace with actual image URL from Unsplash
     },
   ],
 };
